b134d9859cad7d8247af83a3969eab85e18f7690
Shubham Joshi <shubham@playsimple.in>
2021-11-24 08:44:31 +0530

    fixing headers

--- frontend/amundsen_application/oidc_config.py
+++ frontend/amundsen_application/oidc_config.py
@@ -8,12 +8,20 @@ from amundsen_application.config import LocalConfig
 from amundsen_application.models.user import load_user, User
 
 
-def get_access_headers(app):
+def get_access_headers(app: Flask) -> Optional[Dict]:
+    """
+    Function to retrieve and format the Authorization Headers
+    that can be passed to various microservices who are expecting that.
+    :param oidc: OIDC object having authorization information
+    :return: A formatted dictionary containing access token
+    as Authorization header.
+    """
     try:
-        access_token = app.oidc.get_access_token()
+        # noinspection PyUnresolvedReferences
+        access_token = json.dumps(app.auth_client.token)
         return {'Authorization': 'Bearer {}'.format(access_token)}
     except Exception:
-        return None
+        return {}
 
 
 def get_auth_user(app: Flask) -> User:
